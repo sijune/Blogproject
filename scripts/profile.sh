d1397bbac60c00d796e93faa67ecfdcca4a6909e
sijunelee <sijune0525@gmail.com>
2020-12-13 22:35:11 +0900

    CD setup again8

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -22,11 +22,11 @@ function find_idle_profile() {
 }
 
 function find_idle_port() {
-    IDLE_PROFILE=$(find_idle_profile)
+  IDLE_PROFILE=$(find_idle_profile)
 
-    if[ ${IDLE_PROFILE} == real1 ]; then
-      echo "8081"
-    else
-      echo "8082"
-    fi
+  if[ ${IDLE_PROFILE} == real1 ]; then
+    echo "8081"
+  else
+    echo "8082"
+  fi
 }

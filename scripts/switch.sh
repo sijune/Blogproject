b95f831987b309a2d4895a672577b0109a24a880
sijunelee <sijune0525@gmail.com>
2020-12-13 22:18:24 +0900

    CD setup again5

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -12,5 +12,5 @@ function switch_proxy() {
   echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service_url/inc #tee: 파일에도 쓰고 표준출력도 한다.
 
   echo "> 엔진엑스 Reload"
-    sudo service nginx reload # restart는 끊김이 있지만 reload는 끊김이 없다.
+  sudo service nginx reload # restart는 끊김이 있지만 reload는 끊김이 없다.
 }

7ee270683ca011b113d8cf4539f22dcedad01a08
sijunelee <sijune0525@gmail.com>
2020-12-13 21:51:05 +0900

    CD setup

---
+++ scripts/profile.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# 쉬고 있는 profile 찾기
+
+function find_idle_profile() {
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
+
+    # 현재 nginx가 바라보고 있는 profile찾기
+    if [ ${RESPONSE_CODE} -ge 400 ] then
+      CURRENT_PROFILE=real2
+    else
+      CURRENT_PROFILE=$(curl -s http://localhost/profile)
+    fi
+
+    # 쉬고 있는 profile찾기
+    if [ ${CURRENT_PROFILE} == real1 ] then
+      IDLE_PROFILE=real2
+    else
+      IDLE_PROFILE=real1
+    fi
+
+    echo "${IDLE_PROFILE}"
+}
+
+function find_idle_port() {
+    IDLE_PROFILE=$(find_idle_profile)
+
+    if[ ${IDLE_PROFILE} == real1 ] then
+      echo "8081"
+    else
+      echo "8082"
+    fi
+}

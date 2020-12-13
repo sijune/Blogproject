bcc0b8f5dfca2ae1f33a3867917d36a807625c3d
sijunelee <sijune0525@gmail.com>
2020-12-13 21:59:23 +0900

    CD setup again2

--- scripts/health.sh
+++ scripts/health.sh
@@ -17,7 +17,8 @@ do
   RESPONSE=$(curl -s http://localhost:$IDLE_PORT/profile) # 현재 profile
   UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -l)
 
-  if [ ${UP_COUNT} -ge 1 ] then # "real"  문자열이 있다면
+  if [ ${UP_COUNT} -ge 1 ]
+  then # "real"  문자열이 있다면
     echo "> Health check 성공"
     switch_proxy # 프록시 설정 변경
     break
@@ -26,7 +27,8 @@ do
     echo "> Health check: ${RESPONSE}"
   fi
 
-  if [ ${RETRY_COUNT} -eq 10 ] then
+  if [ ${RETRY_COUNT} -eq 10 ]
+  then
     echo "> Health check 실패"
     echo "> 엔진엑스에 연결하지 않고 배포를 종료합니다."
   fi

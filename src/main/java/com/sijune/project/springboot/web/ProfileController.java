55934677ab4640c7ab8a74a706850cf5e0614a1e
sijunelee <sijune0525@gmail.com>
2020-12-13 20:01:53 +0900

    profile Controller Setup again2

--- src/main/java/com/sijune/project/springboot/web/ProfileController.java
+++ src/main/java/com/sijune/project/springboot/web/ProfileController.java
@@ -18,10 +18,13 @@ public class ProfileController {
     public String profile() {
         // 활성화된 profile을 담는다.
         List<String> profiles = Arrays.asList(env.getActiveProfiles());
+
         //filter로 사용될 profile 생성
         List<String> realProfiles = Arrays.asList("real", "real1", "real2");
+
         //profile이 없다면 "default"를, 있다면 첫 profile을 가져온다.
         String defaultProfile = profiles.isEmpty()?"default":profiles.get(0);
+
         //filter로 사용한 profile만 걸러낸다.
         //filter로 사용된 profile내 존재하지 않지만, profile 내 데이터가 있다. => 제일 첫번째 profile
         //filter로 사용된 profile내 존재하지 않지만, profile 내 데이터가 없다. => default
